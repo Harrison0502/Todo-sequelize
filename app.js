ce2659deb333836cd43888530dc5e04c4586be9e
harrison0502 <harrison0502@gmail.com>
2023-07-30 16:35:47 +0800

    Add login & register function

--- app.js
+++ app.js
@@ -12,6 +12,39 @@ app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
   res.render('index')
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+app.post('/users/login',(req,res)=>{
+  res.redirect('/')
+  console.log('login')
+})
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+app.post('/users/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  User.findOne({ where: { email } }).then(user => {
+    if (user) {
+      console.log('User already exists')
+      return res.render('register', {
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    }
+    return bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(password, salt))
+      .then(hash => User.create({
+        name,
+        email,
+        password: hash
+      }))
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
+  })
 })
 
 
